480832302628135732f49671d977f8028f483e67
Diego Ceron <134111842+Zero100x@users.noreply.github.com>
2026-05-07 09:33:53 -0500

    feat: implementa crud roles permisos y revision docente

--- proyectos_julio_william_diego_mike/urls.py
+++ proyectos_julio_william_diego_mike/urls.py
@@ -1,5 +1,21 @@
 from django.urls import path
 
+from .views import (
+    ProyectoCreateView,
+    ProyectoDeleteView,
+    ProyectoDetailView,
+    ProyectoListView,
+    ProyectoRevisionView,
+    ProyectoUpdateView,
+)
+
 app_name = 'proyectos_julio_william_diego_mike'
 
-urlpatterns = []
+urlpatterns = [
+    path('', ProyectoListView.as_view(), name='proyecto_list'),
+    path('crear/', ProyectoCreateView.as_view(), name='proyecto_create'),
+    path('<int:pk>/', ProyectoDetailView.as_view(), name='proyecto_detail'),
+    path('<int:pk>/editar/', ProyectoUpdateView.as_view(), name='proyecto_update'),
+    path('<int:pk>/eliminar/', ProyectoDeleteView.as_view(), name='proyecto_delete'),
+    path('<int:pk>/revisar/', ProyectoRevisionView.as_view(), name='proyecto_revision'),
+]
