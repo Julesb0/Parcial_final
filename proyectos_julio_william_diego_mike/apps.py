9266fffc89f0da508c917b33c19922cb7cce9ef9
Julesb0 <jul.b.benavides@gmail.com>
2026-05-07 08:54:17 -0500

    feat: configura estructura base del proyecto django

---
+++ proyectos_julio_william_diego_mike/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProyectosJulioWilliamDiegoMikeConfig(AppConfig):
+    name = 'proyectos_julio_william_diego_mike'
---
+++ proyectos_julio_william_diego_mike/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+app_name = 'proyectos_julio_william_diego_mike'
+
+urlpatterns = []
